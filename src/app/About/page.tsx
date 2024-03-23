3a4d0cd1fb28df59eee63478d960c2630a4b86c2
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-03-24 06:13:11 +0700

    finalisasi

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -15,6 +15,7 @@ import {
 import Navbar from "../home/navbar";
 import logo from "../img/cup.png";
 import SmallWithSocial from "../home/footer";
+import IndexPage from "./team1";
 
 export default function WithLargeQuote() {
   return (
@@ -50,112 +51,11 @@ export default function WithLargeQuote() {
         </Text>
         <Divider />
         <hr />
-        <Text fontWeight={1000} fontSize={"2xl"} id="Teams">
+        <Text fontWeight={900} fontSize={"2xl"} id="Teams" textAlign={"center"}>
           Meet your (coffee) makers
           <hr />
         </Text>
-        <Box display="flex" flexWrap="wrap" w="85%">
-          <Card maxW="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1599000117646-493e27e0e609?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJhcmlzdGF8ZW58MHx8MHx8fDA%3D"
-                alt="Dan Broom"
-              />
-              <Stack mt="6" spacing="3" textAlign="center">
-                <Heading size="md">Dan Broom</Heading>
-                <Text>
-                  Our valuable head bar at our Cafe, incredible work rate and
-                  awesome management.
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-          <Card maxW="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1507914464562-6ff4ac29692f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGJhcmlzdGF8ZW58MHx8MHx8fDA%3D"
-                alt="Claire Bossum"
-              />
-              <Stack mt="6" spacing="3" textAlign="center">
-                <Heading size="md">Claire Bossum</Heading>
-                <Text>
-                  Our most playful barista, she had completed the task of most
-                  great barista ever achieve.
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-          <Card maxW="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1595475038784-bbe439ff41e6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJhcmlzdGF8ZW58MHx8MHx8fDA%3D"
-                alt="Franz Kehler"
-              />
-              <Stack mt="6" spacing="3" textAlign="center">
-                <Heading size="md">Franz Kehler</Heading>
-                <Text>
-                  Lovely guy who determined in doing his job, always cheered
-                  other employee which makes him the best waiters in our Cafe.
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-          <Card maxW="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1599549192669-6baac5d32432?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGJhcmlzdGF8ZW58MHx8MHx8fDA%3D"
-                alt="Fredrick Bohler"
-              />
-              <Stack mt="6" spacing="3" textAlign="center">
-                <Heading size="md">Fredrick Bohler</Heading>
-                <Text>
-                  If there is a word for "Most focused Man on earth" I will give
-                  that to this guy, he's awesome time management and precision
-                  says it all.
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-          <Card maxW="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1517701928392-16651ac1e944?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGJhcmlzdGF8ZW58MHx8MHx8fDA%3D"
-                alt="Justin Harrison"
-              />
-              <Stack mt="6" spacing="3" textAlign="center">
-                <Heading size="md">Justin Harrison</Heading>
-                <Text>
-                  Our employee of the month, Justin make his work like its his
-                  playground always know the detail in everything. Although food
-                  is his main objective but his workrate in covering other
-                  employee is exceptional.
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-          <Card maxW="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1517701848373-805868efd0ac?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGJhcmlzdGF8ZW58MHx8MHx8fDA%3D"
-                alt="Herera Hawk"
-              />
-              <Stack mt="6" spacing="3" textAlign="center">
-                <Heading size="md">Herera Hawk</Heading>
-                <Text>
-                  Our new Barista from Starling, with experience above the roof
-                  his specialty brings joy to our customers. Great sense of
-                  Humor and competent in Cashier Machine.
-                </Text>
-              </Stack>
-            </CardBody>
-            <Divider />
-          </Card>
-        </Box>
+        <IndexPage />
         <br />
         <Divider />
         <Text
---
+++ src/app/About/team1.tsx
@@ -0,0 +1,77 @@
+// pages/index.tsx
+
+import {
+  Avatar,
+  Stack,
+  Text,
+  Card,
+  CardBody,
+  Heading,
+  Divider,
+} from "@chakra-ui/react"; // pages/index.tsx
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface User {
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  email: string;
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+}
+const IndexPage = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchRandomUsers = async () => {
+      try {
+        const response = await axios.get(
+          "https://randomuser.me/api/?results=6"
+        );
+        setUsers(response.data.results);
+      } catch (error) {
+        console.error("Error fetching random users:", error);
+      }
+    };
+
+    fetchRandomUsers();
+
+    // Clean up function to cancel the request (if necessary)
+    return () => {
+      // cancel request or cleanup as needed
+    };
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {users.map((user, index) => (
+          <div key={index}>
+            <Card maxW={"sm"} textAlign={"center"} p={5} m={2}>
+              <CardBody>
+                <Avatar src={user.picture.large} boxSize={"200px"} />
+
+                <Stack mt="2" spacing="3" textAlign="center">
+                  <Heading size={"md"}>
+                    {user.name.title}. {user.name.first} {user.name.last}
+                  </Heading>
+                  <Text>{user.email}</Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
