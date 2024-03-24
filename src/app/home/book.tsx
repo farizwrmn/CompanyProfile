aa7780bd189715712da45d17304140ecc83204f5
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-03-24 10:13:49 +0700

    finalisasi

--- src/app/home/book.tsx
+++ src/app/home/book.tsx
@@ -112,7 +112,7 @@ export default function Contact() {
                         </InputGroup>
                       </FormControl>
                       <FormControl id="name" isRequired>
-                        <FormLabel>Mail</FormLabel>
+                        <FormLabel>E=Mail</FormLabel>
                         <InputGroup borderColor="#E0E1E7">
                           <InputLeftElement pointerEvents="none">
                             <MdOutlineEmail color="gray.800" />
