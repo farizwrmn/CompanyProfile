669b54954a5e989110d0bfb270b18c06355b94ae
Muhamad Fariz Warman <farizwarman@gmail.com>
2024-03-24 10:07:19 +0700

    finalisasi

--- src/app/home/testimonial.tsx
+++ src/app/home/testimonial.tsx
@@ -129,6 +129,7 @@ export default function WithSpeechBubbles() {
               }
               name={"Jane Cooper"}
               title={"COO at ABC Corporation"}
+              alt="Jane Cooper"
             />
           </Testimonial>
           <Testimonial>
@@ -147,6 +148,7 @@ export default function WithSpeechBubbles() {
               }
               name={"Dorothy Aridan"}
               title={"PM at AAC Corporation"}
+              alt="Dorothy Aridan"
             />
           </Testimonial>
           <Testimonial>
@@ -164,6 +166,7 @@ export default function WithSpeechBubbles() {
               }
               name={"Gill Dollar"}
               title={"Team Leader at SiPecat"}
+              alt="Gill Dollar"
             />
           </Testimonial>
         </Stack>
